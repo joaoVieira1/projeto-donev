d074d1e62f88a0ef859fc9e466c7acc23ada2b97
João Pedro da Silva Vieira AQ3022366 <vieira.joao1@aluno.ifsp.edu.br>
2025-04-23 11:19:31 -0300

    project creation

---
+++ src/main/java/com/application/donev/project/DonevProjectApplication.java
@@ -0,0 +1,13 @@
+package com.application.donev.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonevProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonevProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/donev/project/DonevProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.donev.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonevProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
