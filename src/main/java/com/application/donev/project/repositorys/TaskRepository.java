da9f2f6203f614d476f371e7693999efbab4eca8
João Pedro da Silva Vieira AQ3022366 <vieira.joao1@aluno.ifsp.edu.br>
2025-04-23 16:37:12 -0300

    arranging repositories

--- src/main/java/com/application/donev/project/repositorys/TaskRepository.java
+++ src/main/java/com/application/donev/project/repositorys/TaskRepository.java
@@ -3,6 +3,6 @@ package com.application.donev.project.repositorys;
 import com.application.donev.project.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Long, Task> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 
 }
--- src/main/java/com/application/donev/project/repositorys/UserRepository.java
+++ src/main/java/com/application/donev/project/repositorys/UserRepository.java
@@ -3,7 +3,7 @@ package com.application.donev.project.repositorys;
 import com.application.donev.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 
 }
